b258b4f6714af1bb81b259aefd520cb4348dd4ad
iustina-d <iustina2dulea@gmail.com>
2023-01-11 12:25:48 +0200

    add more routes

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,session
+from flask import Flask,render_template, request, session, redirect, url_for
 import os
 import data_manager
 
@@ -15,7 +15,19 @@ def main():
   get_books = data_manager.get_books()
   return render_template("home.html",books=get_books)
 
+@app.route("/<page>")
+def get_books_using_page_number(page):
+  page = int(page)
+  get_books = data_manager.get_books_on_page(page)
+  return render_template("home.html",books=get_books,page=page)
 
+@app.route("/search-bar" ,methods=["POST"])
+def get_books_sing_search_bar():
+  text = request.form.get("myInput")
+  # text = input.capitalize()
+  # text = str(text)
+  get_books = data_manager.get_books_using_search_box(text)
+  return render_template("home.html",books=get_books)
 
 if __name__ == "__main__":
     app.run(debug=True)
