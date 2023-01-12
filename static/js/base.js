79629ebb36c07558884f3673cb2624a68e3f6b07
iustina-d <iustina2dulea@gmail.com>
2023-01-13 00:13:38 +0200

    make the badge work when the add button is clicked

--- static/js/base.js
+++ static/js/base.js
@@ -1,17 +1,10 @@
-//search-bar
-function myFunction() {
-  var input, filter, ul, li, a, i, txtValue
-  input = document.getElementById("myInput")
-  filter = input.value.toUpperCase()
-  ul = document.getElementById("myUL")
-  li = ul.getElementsByTagName("li")
-  for (i = 0; i < li.length; i++) {
-    a = li[i].getElementsByTagName("a")[0]
-    txtValue = a.textContent || a.innerText
-    if (txtValue.toUpperCase().indexOf(filter) > -1) {
-      li[i].style.display = ""
-    } else {
-      li[i].style.display = "none"
-    }
-  }
-}
+var number = 0;
+const buttons = document.querySelectorAll(".badge_message")
+
+buttons.forEach(button => {
+  button.addEventListener("click", function() {
+    number ++;
+    console.log(number);
+    document.querySelector(".badge").innerHTML = number;
+  });
+});
